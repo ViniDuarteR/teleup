2cb5b4b38f9087357570f0545c868400cb9096dc
EuHttl <hyttalo2002@gmail.com>
2025-10-14 09:35:41 -0300

    fix: caminho do arquivo do NotFound

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -12,7 +12,7 @@ const NotFound = () => {
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="text-center">
         <h1 className="mb-4 text-4xl font-bold">404</h1>
-        <img src="/assets/images/404.png" alt="404" className="w-1/2 h-1/2" />
+        <img src="/images/page_not_found.jpg" alt="404" className="w-1/2 h-1/2" />
         <p className="mb-4 text-xl text-gray-600">Oops! Essa pagina ainda está em desenvolvimento</p>
         <a href="/" className="text-blue-500 underline hover:text-blue-700">
           Voltar para a página inicial
