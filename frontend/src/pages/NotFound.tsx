09277f7ca21aea64d17290d1bd8c767a6bef03b8
EuHttl <hyttalo2002@gmail.com>
2025-10-14 09:25:06 -0300

    feat: adicionando imagem a page not found

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -12,9 +12,10 @@ const NotFound = () => {
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="text-center">
         <h1 className="mb-4 text-4xl font-bold">404</h1>
-        <p className="mb-4 text-xl text-gray-600">Oops! Page not found</p>
+        <img src="/assets/images/404.png" alt="404" className="w-1/2 h-1/2" />
+        <p className="mb-4 text-xl text-gray-600">Oops! Essa pagina ainda está em desenvolvimento</p>
         <a href="/" className="text-blue-500 underline hover:text-blue-700">
-          Return to Home
+          Voltar para a página inicial
         </a>
       </div>
     </div>
